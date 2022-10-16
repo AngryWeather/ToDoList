12c7de45c667240ba4238c2b64acd2ba23890ed9
Weronika <WerRic96@protonmail.com>
2022-10-16 20:00:27 +0200

    Add heading to header

--- src/header.js
+++ src/header.js
@@ -2,6 +2,14 @@ function createHeader() {
     const body = document.querySelector("body");
     const header = document.createElement("header");
     body.appendChild(header);
+
+    header.appendChild(createHeadingOne());
+}
+
+function createHeadingOne() {
+    const h1 = document.createElement("h1");
+    h1.textContent = "Just Do It";
+    return h1;
 }
 
 export{ createHeader };
