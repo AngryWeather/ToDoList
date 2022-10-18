5a2018b1f031d40f6e1b54421567028df34a5118
Weronika <WerRic96@protonmail.com>
2022-10-18 21:00:57 +0200

    Add event listener to new task

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,28 @@
 import { generateProject } from "./mainContent";
 
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.tasks = [];
+    }
+}
+
+class Task {
+    constructor(description, dueDate, priority) {
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+// function defaultPersonalProject() {
+
+// }
+
+function addEventListenerNewTask() {
+    const newTaskButton = document.querySelector(".heading-button-container > button");
+    newTaskButton.addEventListener("click", newTaskPopUp);
+}
 
 function addEventListenerProjects() {
     const projectButtons = document.querySelectorAll(".project-buttons");
